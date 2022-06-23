032a81f337741ace757f63ee10b314850ff6fc1c
Chaitanya1496 <chaitanyashejwal26@gmail.com>
2022-06-23 07:43:25 +0530

    Excel read and configurable variable assignment

--- Engine/AlertDetection.r
+++ Engine/AlertDetection.r
@@ -6,20 +6,25 @@ library(xlsx)
 # Logic run
 
 # Data Extraction
-data <- read.xlsx("../Data/CIB_Data.xlsx")
+readData <- function() {
+    configData <<- read.xlsx("../Data/CIB_Data.xlsx", sheetIndex <- 3)
+    transactionData <<- read.xlsx("../Data/CIB_Data.xlsx", sheetIndex <- 1)
+}
 
 # Configurations
-# Tunable
-displayAllTransaction <<- ""
-excludeCreditActivity <<- ""
-minNumberSD <<- ""
-maxNumberSD <<- ""
-minCurrentMonthAmt <<- ""
-minPercentageIncrease <<- ""
-includeMonthsWithNoActivity <<- ""
-lookBackPeriod <<- ""
-minOpenDays <<- ""
+populateConfigurationParameters <- function() {
+    # Tunable
+    displayAllTransaction <<- configData$DisplayAllTransaction
+    excludeCreditActivity <<- configData$ExcludeCreditActivity
+    minNumberSD <<- configData$MinNumberSD
+    maxNumberSD <<- configData$MaxNumberSD
+    minCurrentMonthAmt <<- configData$MinCurrentMonthAmt
+    minPercentageIncrease <<- configData$MinPercentageIncrease
+    includeMonthsWithNoActivity <<- configData$IncludeMonthsWithNoActivity
+    lookBackPeriod <<- configData$LookbackPeriod
+    minOpenDays <<- configData$MinOpenDays
 
-# Non Tunable
-# Daily = "D", Monthly = "M", Weekly = "W", Yearly = "Y"
-frequencyPeriod <<- "M"
+    # Non Tunable
+    # Daily = "D", Monthly = "M", Weekly = "W", Yearly = "Y"
+    frequencyPeriod <<- "M"
+}
