7da271eef1eb88c478396c5ce20abd46db6b06da
Chaitanya1496 <chaitanyashejwal26@gmail.com>
2022-06-23 07:22:21 +0530

    Configuration Variables Declared

---
+++ Engine/AlertDetection.r
@@ -0,0 +1,21 @@
+# Data Extraction
+# Configurable parameters fill
+# Logic run
+
+# Data Extraction
+
+# Configurations
+# Tunable
+displayAllTransaction <<- ""
+excludeCreditActivity <<- ""
+minNumberSD <<- ""
+maxNumberSD <<- ""
+minCurrentMonthAmt <<- ""
+minPercentageIncrease <<- ""
+includeMonthsWithNoActivity <<- ""
+lookBackPeriod <<- ""
+minOpenDays <<- ""
+
+# Non Tunable
+# Daily = "D", Monthly = "M", Weekly = "W", Yearly = "Y"
+frequencyPeriod <<- "M"
