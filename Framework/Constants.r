6cd328b7ec8f647672a5ebd5d663380750275fa2
Chaitanya1496 <chaitanyashejwal26@gmail.com>
2022-07-20 11:44:39 +0530

    Added a few constants

--- Framework/Constants.r
+++ Framework/Constants.r
@@ -4,5 +4,7 @@ inBoundCreditType <- c("T", "t", "TRUE", "True", "true", TRUE)
 inBoundCredit <- "TRUE"
 inBoundDebitType <- c("F", "f", "FALSE", "False", "false", FALSE)
 inBoundDebit <- "FALSE"
+abnormalBehaviour <- "Abnormal"
+normalBehaviour <- "Normal"
 # Daily = "D", Monthly = "M", Weekly = "W", Yearly = "Y"
 monthlyFrequency <- "M"
